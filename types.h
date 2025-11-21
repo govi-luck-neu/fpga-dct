40a27f7757b59ceb175824b1d668fb1dbef6e2e4
govi-luck-neu <luck.g@northeastern.edu>
2025-11-20 20:16:04 -0500

    final type changes

--- types.h
+++ types.h
@@ -4,10 +4,14 @@
 */
 // #include <ap_int.h>
 // #include <ap_fixed.h>
+
 // typedef ap_int<5> data_t;
 typedef double	data_t;
+
 typedef int	coef_t;
-// typedef ap_fixed<24, 8> calc_t
+// typedef ap_int<5> coef_t;
+
+// typedef ap_fixed<30, 16> calc_t
 typedef double calc_t;
 
 #ifndef IDX
